0222ab55c81bb75d4cf507fd27a507ac16398548
paulwmbrg <paul.wambergue@gmail.com>
2017-06-13 17:13:13 -0400

    splitting latticetester/Const.h in two separate files: latticetester/Const.h and latmrg/Const.h

--- src/Const.cc
+++ src/Const.cc
@@ -42,47 +42,6 @@ string toStringNorm (NormType norm)
    }
 }
 
-
-//===========================================================================
-
-string toStringGen (GenType gen)
-{
-   switch (gen) {
-   case MWC:
-      return "MWC";
-   case LCG:
-      return "LCG";
-   case MRG:
-      return "MRG";
-   case KOROBOV:
-      return "KOROBOV";
-   case RANK1:
-      return "RANK1";
-   default:
-      return "***** GenType: IMPOSSIBLE CASE ";
-   }
-}
-
-
-//===========================================================================
-
-string toStringLattice (LatticeType lat)
-{
-   switch (lat) {
-   case FULL:
-      return "FULL";
-   case RECURRENT:
-      return "RECURRENT";
-   case ORBIT:
-      return "ORBIT";
-   case PRIMEPOWER:
-      return "PRIMEPOWER";
-   default:
-      return "***** LatticeType: IMPOSSIBLE CASE ";
-   }
-}
-
-
 //===========================================================================
 
 string toStringPrime (PrimeType stat)
@@ -98,45 +57,6 @@ string toStringPrime (PrimeType stat)
       return "UNKNOWN";
    }
 }
-
-
-//===========================================================================
-
-string toStringDecomp (DecompType deco)
-{
-   switch (deco) {
-   case DECOMP:
-      return "DECOMP";
-   case DECOMP_WRITE:
-      return "DECOMP_WRITE";
-   case DECOMP_READ:
-      return "DECOMP_READ";
-   case DECOMP_PRIME:
-      return "DECOMP_PRIME";
-   default:
-      return "***** DecompType: IMPOSSIBLE CASE ";
-   }
-}
-
-
-//===========================================================================
-
-string toStringOutput (OutputType sort)
-{
-   switch (sort) {
-   case TERMINAL:
-      return "TERMINAL";
-   case RES:
-      return "RES";
-   case TEX:
-      return "TEX";
-   case GEN:
-      return "GEN";
-   default:
-      return "***** OutputType: IMPOSSIBLE CASE ";
-   }
-}
-
 
 //===========================================================================
 
@@ -195,43 +115,6 @@ string toStringCalc (CalcType calc)
       return "***** CalcType: IMPOSSIBLE CASE ";
    }
 }
-
-
-//===========================================================================
-
-string toStringImplemCond (ImplemCond cond)
-{
-   switch (cond) {
-   case NO_COND:
-      return "NO_COND";
-   case APP_FACT:
-      return "APP_FACT";
-   case EQUAL_COEF:
-      return "EQUAL_COEF";
-   case ZERO_COEF:
-      return "ZERO_COEF";
-   case POWER_TWO:
-      return "POWER_TWO";
-   default:
-      return "***** ImplemCond: IMPOSSIBLE CASE ";
-   }
-}
-
-
-//===========================================================================
-
-string toStringSearchMethod (SearchMethod method)
-{
-   switch (method) {
-   case EXHAUST:
-      return "EXHAUST";
-   case RANDOM:
-      return "RANDOM";
-   default:
-      return "***** SearchMethod: IMPOSSIBLE CASE ";
-   }
-}
-
 
 //===========================================================================
 

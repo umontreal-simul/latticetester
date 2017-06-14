11a264388dce05c96d8301700ae80ff7275d1ae0
paulwmbrg <paul.wambergue@gmail.com>
2017-06-14 15:29:35 -0400

    de-dehumanize

--- src/Normalizer.cc
+++ src/Normalizer.cc
@@ -34,100 +34,99 @@
 namespace LatticeTester
 {
 
-   const int Normalizer::MAX_DIM;
+const int Normalizer::MAX_DIM;
 
-   /*-------------------------------------------------------------------------*/
+/*-------------------------------------------------------------------------*/
 
-   Normalizer::Normalizer (const MScal & m0, int k0, int maxDim, std::string name,
-                           NormType norm, double beta0) :
-         m_name(name), m_norm(norm), m_m(m0), m_rank(k0), m_maxDim(maxDim),
-         m_beta(beta0)
-   {
-      m_cst = new double[maxDim];
+Normalizer::Normalizer (const MScal & m0, int k0, int maxDim, std::string name,
+                        NormType norm, double beta0) :
+      m_name(name), m_norm(norm), m_m(m0), m_rank(k0), m_maxDim(maxDim),
+      m_beta(beta0)
+{
+   m_cst = new double[maxDim + 1];
+}
+
+
+/*-------------------------------------------------------------------------*/
+
+
+//PW_TODO : why not including this init method in the constructor ?
+
+
+void Normalizer::init (const MScal &m0, int k0, double beta0)
+/*
+ * Computes the vector Cst that corresponds to G, for a lattice of
+ * density m^k for t >= k, and m^t for t < k.
+ */
+{
+   double x, y;
+   double logBeta;
+   double logm;
+   double k = k0;
+   m_rank = k0;
+   m_m = m0;
+   m_beta = beta0;
+
+   y = 1.0;
+   logBeta = log (m_beta);
+#ifdef WITH_NTL
+   logm = log(NTL::to_ZZ(m_m));
+#else
+   logm = log(m_m);
+#endif
+
+   //PW_TODO: check indices and impact on getGamma that starts at 1
+
+   for (int j = 1; j <= m_maxDim; j++) { //fred
+      if (j > k)
+         y = k / j;
+
+      x = log (getGamma(j)) + j * logBeta + y * logm;
+      //log calculation because m can be large
+
+      if (m_norm == L2NORM) // is L2NORM always used squarred?
+         x = x + x;
+      m_cst[j] = exp (x);
    }
+}
 
 
-   /*-------------------------------------------------------------------------*/
+/*-------------------------------------------------------------------------*/
 
+std::string Normalizer::ToString () const
+{
+   std::ostringstream os;
+   os << "-----------------------------\n"
+   << "Content of Normalizer object:\n\n Normalizer = " << m_name;
+   os << "\n m = " << m_m;
+   os << "\n rank = " << m_rank;
+   os << "\n beta = " << std::setprecision (4) << m_beta << "\n\n";
 
-   //PW_TODO : why not including this init method in the constructor ?
-
-   void Normalizer::init (const MScal &m0, int k0, double beta0)
-   /*
-    * Computes the vector Cst that corresponds to G, for a lattice of
-    * density m^k for t >= k, and m^t for t < k.
-    */
-   {
-      double x, y;
-      double logBeta;
-      double logm;
-      double k = k0;
-      m_rank = k0;
-      m_m = m0;
-      m_beta = beta0;
-
-      y = 1.0;
-      logBeta = log (m_beta);
-   #ifdef WITH_NTL
-      logm = log(NTL::to_ZZ(m_m));
-   #else
-      logm = log(m_m);
-   #endif
-
-      //PW_TODO: check indices and impact on getGamma that starts at 1
-
-      for (int j = 0; j < m_maxDim; j++) {
-         if (j > k-1)
-            y = k / (j+1);
-
-         x = log (getGamma(j+1)) + (j+1) * logBeta + y * logm;
-         //log calculation because m can be large
-
-         if (m_norm == L2NORM) //L2NORM always used squarred?
-            x *= 2;
-         
-         m_cst[j] = exp (x);
-      }
+   //   os.setf(std::ios::left);
+   os << std::setprecision (13);
+   for (int t = 1; t <= m_maxDim; t++) { //fred
+      os << " Cst[" << std::setw(2) << std::right << t << "] = "
+      << std::setw(14) << std::left << m_cst[t] << "\n";
    }
+   os << "\n";
+   return os.str ();
+}
 
 
-   /*-------------------------------------------------------------------------*/
+/*-------------------------------------------------------------------------*/
 
-   std::string Normalizer::ToString () const
-   {
-      std::ostringstream os;
-      os << "-----------------------------\n";
-      os << "Content of Normalizer object:\n\n Normalizer = " << m_name;
-      os << "\n m = " << m_m;
-      os << "\n rank = " << m_rank;
-      os << "\n beta = " << std::setprecision (4) << m_beta << "\n\n";
-
-      //os.setf(std::ios::left);
-
-      os << std::setprecision (13);
-      for (int t = 0; t < m_maxDim; t++) {
-         os << " Cst[" << std::setw(2) << std::right << t << "] = "
-         << std::setw(14) << std::left << m_cst[t] << "\n";
-      }
-      os << "\n";
-      return os.str ();
-   }
+double Normalizer::getGamma (int) const
+{
+   return 1.0;
+}
 
 
-   /*-------------------------------------------------------------------------*/
+/*-------------------------------------------------------------------------*/
 
-   double Normalizer::getGamma (int) const
-   {
-      return 1.0;
-   }
-
-
-   /*-------------------------------------------------------------------------*/
-
-   double & Normalizer::getCst (int j)
-   {
-      assert (j >= 0 && j < m_maxDim);
-      return m_cst[j];
-   }
+double & Normalizer::getCst (int j)
+{
+   assert (j >= 1 && j <= m_maxDim); //fred
+   return m_cst[j];
+}
 
 }

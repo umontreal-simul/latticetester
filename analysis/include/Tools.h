f3f82baef03aa1ae177a7fcd750909b23a82fb71
paulwmbrg <paul.wambergue@gmail.com>
2017-07-05 11:35:44 -0400

    adding the vector of coefficients a to the parameters of the function CreateRNGBasis

--- analysis/include/Tools.h
+++ analysis/include/Tools.h
@@ -163,7 +163,7 @@ vec_ZZ randomVector (int dimension, ZZ modulus, ZZ seed)
    return vector;
 }
 
-mat_ZZ CreateRNGBasis (const ZZ modulus, const int order, const int dimension, ZZ seed)
+mat_ZZ CreateRNGBasis (const ZZ modulus, const int order, const int dimension, vec_ZZ& a, ZZ seed)
 {
    mat_ZZ basis;
    basis.SetDims(dimension,dimension);
@@ -176,8 +176,8 @@ mat_ZZ CreateRNGBasis (const ZZ modulus, const int order, const int dimension, Z
    } else { //usual case
 
       // (a_i) coefficients
-      vec_ZZ a;
-      a = randomVector(order, modulus, seed);
+      //vec_ZZ a;
+      //a = randomVector(order, modulus, seed);
 
       for (int i = 0; i < order; i++) {
          // left upper block

391ad4827696cf81acbfe4bf20aa383812a3f47b
paulwmbrg <paul.wambergue@gmail.com>
2017-08-09 14:25:10 -0400

    adding both primal and dual basis for code debugging

--- analysis/src/routinesTest.cc
+++ analysis/src/routinesTest.cc
@@ -26,8 +26,6 @@ using namespace LatticeTester;
 
 //==================================================================================
 
-
- 
 int main (int argc, char *argv[])
 {
    // all the parameters
@@ -35,7 +33,7 @@ int main (int argc, char *argv[])
    NormType norm = L2NORM;
    NormaType normalizerType = BESTLAT;
    long maxNodesBB = 5;
-   PreReductionType preRed = LenstraLL;
+   PreReductionType preRed = BKZ; //LenstraLL;
    PrecisionType doublePrecision = QUADRUPLE; 
    double fact = 0.9;
    int blocksize = 10;
@@ -49,6 +47,7 @@ int main (int argc, char *argv[])
    */
    
    matrix.resize(8,8);
+   //dual
    matrix[0][0]=9223372036854773561;    
    matrix[1][1]=9223372036854773561;   
    matrix[2][2]=9223372036854773561;
@@ -62,8 +61,25 @@ int main (int argc, char *argv[])
    matrix[5][0]=-7721957468801369126; matrix[5][1]=-6629950407641799081; matrix[5][2]=-2092544303070727312;
    matrix[6][0]=-8814617570338055415; matrix[6][1]=-7721957468801369126; matrix[6][2]=-1127730827481453799;
    matrix[7][0]=-3020738662083495906; matrix[7][1]=-8814617570338055415; matrix[7][2]=-1975258172869346907;
-
-   //cout << "matrix = \n" << matrix << endl;
+   //primal
+   /*
+   matrix[0][0]=1;
+   matrix[1][1]=1;
+   matrix[2][2]=1;
+   matrix[3][3]=9223372036854773561;
+   matrix[4][4]=9223372036854773561;
+   matrix[5][5]=9223372036854773561;
+   matrix[6][6]=9223372036854773561;
+   matrix[7][7]=9223372036854773561;
+   matrix[0][3]=9222187883300163885; matrix[1][3]=0;                   matrix[2][3]=1145902849652723;
+   matrix[0][4]=6629950407641799081; matrix[1][4]=9222187883300163885; matrix[2][4]=6821832294614679088;
+   matrix[0][5]=7721957468801369126; matrix[1][5]=6629950407641799081; matrix[2][5]=2092544303070727312;
+   matrix[0][6]=8814617570338055415; matrix[1][6]=7721957468801369126; matrix[2][6]=1127730827481453799;
+   matrix[0][7]=3020738662083495906; matrix[1][7]=8814617570338055415; matrix[2][7]=1975258172869346907;
+   */
+   
+   
+   cout << "matrix = \n" << matrix << endl;
 
    // printing the length of shortest vector
    cout << "Length = " << ShortestVector(matrix, norm) << endl; //, preRed, doublePrecision, fact, blocksize) << endl;
@@ -71,8 +87,10 @@ int main (int argc, char *argv[])
    // printing the FoM
    cout << "FoM = " << FigureOfMerit(matrix, normalizerType) << endl; //, preRed, doublePrecision, blocksize) << endl;
    
+   // printing the FoM Beyer
+   cout << "FoM Beyer = " << FigureOfMeritBeyer(matrix) << endl; //, preRed, doublePrecision, blocksize) << endl;
+   
    return 0;
 }
- 
 
 

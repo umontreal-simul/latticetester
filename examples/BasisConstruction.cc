af14c565b81e759b6962c193ba496ea56119ff8e
Marc-Antoine Savard <ma2112savard@gmail.com>
2019-03-26 15:47:17 -0400

    Changing BasisConstruction to also showcase IntLatticeBasis construction.

--- examples/BasisConstruction.cc
+++ examples/BasisConstruction.cc
@@ -45,42 +45,6 @@ namespace {
   void testFunction(void (BasisConstruction<BScal>::*func)(BMat&),
       BasisConstruction<BScal>& constr, clock_t time1[], clock_t time2[],
       int max_dim){
-    clock_t tmp;
-    for (int j = 0; j < max_dim; j++) {
-      for (int k = 0; k < 10; k++) {
-        tmp = clock();
-        // Reader shenanigans
-        std::string name = "bench/" + prime + "_" + std::to_string((j+1)*5) + "_" + std::to_string(k);
-        ParamReader<MScal, BScal, RScal> reader(name + ".dat");
-        reader.getLines();
-        int numlines;
-        unsigned int ln;
-        reader.readInt(numlines, 0, 0);
-        BMat bas_mat, dua_mat;
-        bas_mat.SetDims(numlines, numlines);
-        dua_mat.SetDims(numlines, numlines);
-        ln = 1;
-        reader.readBMat(bas_mat, ln, 0, numlines);
-
-        // We want to avoid singular matrix because we can't compute the dual, and
-        // IntLatticeBasis only really supports square matrices.
-        if (NTL::determinant(bas_mat) == 0) {
-          std::cout << name << " is singular\n";
-          continue;
-        }
-
-        // Timing ma first
-        (constr.*func)(bas_mat);
-        // If you don't need the dual basis, the following line is sufficient
-        // basis = IntLatticeBasis<MScal, BScal, NScal, RScal>(matrix, numlines);
-        MScal modulo(1);
-        time1[j] += clock() - tmp;
-
-        tmp = clock();
-        constr.DualConstruction(bas_mat, dua_mat, modulo);
-        time2[j] += clock() - tmp;
-      }
-    }
   }
 }
 
@@ -98,16 +62,66 @@ int main() {
     dual2_time[i] = 0;
   }
 
-  ParamReader<MScal, BScal, RScal> reader;
-
   // Defining constants for the execution of the algorithms
   BasisConstruction<BScal> constr; // The basis constructor we will use
   BMat bas_mat, dua_mat;
 
-  testFunction(&BasisConstruction<BScal>::GCDConstruction, constr, gcd_time,
-      dual1_time, max_dim);
-  testFunction(&BasisConstruction<BScal>::LLLConstruction, constr, lll_time,
-      dual2_time, max_dim);
+  //testFunction(&BasisConstruction<BScal>::GCDConstruction, constr, gcd_time,
+  //    dual1_time, max_dim);
+  //testFunction(&BasisConstruction<BScal>::LLLConstruction, constr, lll_time,
+  //    dual2_time, max_dim);
+  clock_t tmp;
+  for (int j = 0; j < max_dim; j++) {
+    for (int k = 0; k < 10; k++) {
+      // Reader shenanigans
+      std::string name = "bench/" + prime + "_" + std::to_string((j+1)*5) + "_" + std::to_string(k);
+      ParamReader<MScal, BScal, RScal> reader(name + ".dat");
+      reader.getLines();
+      int numlines;
+      unsigned int ln;
+      reader.readInt(numlines, 0, 0);
+      BMat bas_mat, dua_mat;
+      bas_mat.SetDims(numlines, numlines);
+      dua_mat.SetDims(numlines, numlines);
+      ln = 1;
+      reader.readBMat(bas_mat, ln, 0, numlines);
+
+      // Creating a lattice basis
+      IntLatticeBasis<MScal, BScal, NScal, RScal> lattice(bas_mat, numlines);
+
+      // We want to avoid singular matrix because we can't compute the dual, and
+      // IntLatticeBasis only really supports square matrices.
+      if (NTL::determinant(bas_mat) == 0) {
+        std::cout << name << " is singular\n";
+        continue;
+      }
+
+      // Timing GCD first
+      tmp = clock();
+      constr.GCDConstruction(bas_mat);
+      MScal modulo(1);
+      gcd_time[j] += clock() - tmp;
+
+      tmp = clock();
+      constr.DualConstruction(bas_mat, dua_mat, modulo);
+      dual1_time[j] += clock() - tmp;
+
+      // Timing LLL next
+      tmp = clock();
+      constr.LLLConstruction(lattice.getBasis());
+      modulo = MScal(1);
+      lll_time[j] += clock() - tmp;
+
+      // The following works, but does not set all the properties of lattice to
+      // properly work with a dual.
+      tmp = clock();
+      constr.DualConstruction(lattice.getBasis(), lattice.getDualBasis(), modulo);
+      dual2_time[j] += clock() - tmp;
+      // This sets the lattice to know it has a dual. Computing the norm of the
+      // vectors in the lattice would also be wise.
+      lattice.setDualFlag(true);
+    }
+  }
 
   std::cout << "         ";
   int width1 = getWidth(gcd_time, max_dim, "GCD", totals, 0);
